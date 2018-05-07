dcdbf6cd9b48f56a77e3999e3db04b40641028c1
MELANIE SEAN <msean@a204-18.iut.univ-paris8.fr>
2018-05-07 16:33:39 +0200

    zrz

--- src/app/Main.java
+++ src/app/Main.java
@@ -22,6 +22,7 @@ public class Main extends Application {
 		
 		
 		root=loader.load();
+		System.out.println(root);
      	Scene scene = new Scene(root);
 		primaryStage.setScene(scene);
 		primaryStage.show();
--- src/controleur/ControleurZelda.java
+++ src/controleur/ControleurZelda.java
@@ -1,10 +1,13 @@
 package controleur;
 
+
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.TilePane;
@@ -14,18 +17,25 @@ import modele.Plateau;
 public class ControleurZelda implements Initializable{
 	
 	@FXML
-    private TilePane tuiles;
-	 Image image = new Image("/home/etudiants/info/msean/Images/flower.jpg");
+    private TilePane tuiles = new TilePane();
+	
+	String efrefr = new File("src/vue/tileset.png").toURI().toString();
+	 Image image = new Image(efrefr);
+
 	
 
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
+		System.out.println("gdyzeu");
 		Plateau unPlateau = new Plateau();
 		for(int x = 0; x<12; x++) {
 			for(int y = 0; y<12; y++) {
 				if(unPlateau.getLePlateau()[x][y] == 'm') {
-					tuiles.getChildren().add(new ImageView(image));
+					System.out.println(unPlateau.getLePlateau()[x][y]);
+					ImageView temp = new ImageView(image);
+					temp.setViewport(new Rectangle2D(0, 0, 16, 16));
+					tuiles.getChildren().add(temp);
 				}
 			}
 			
