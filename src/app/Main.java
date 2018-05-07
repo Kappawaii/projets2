aa123f31e7eaa44de2813537417bab94c6b986fb
MELANIE SEAN <msean@a204-18.iut.univ-paris8.fr>
2018-05-07 14:56:56 +0200

    ajout src

---
+++ src/app/Main.java
@@ -0,0 +1,37 @@
+package app;
+
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+	@FXML
+	BorderPane root;
+
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(Main.class.getResource("../vue/essai.fxml"));
+		
+		
+		root=loader.load();
+     	Scene scene = new Scene(root);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
---
+++ src/modele/Plateau.java
@@ -0,0 +1,27 @@
+package modele;
+
+import javafx.scene.image.ImageView;
+
+public class Plateau {
+
+	/*
+	 * 'a' : arbre
+	 * 'n' : case noir, vide?
+	 * 'b' : blanc
+	 */
+	
+
+	char notreMap[][] = { {'c','c','c','c','c','c','c','c','c','c','c','c'},
+						  {'c','c','c','c','c','c','c','c','c','c','c','c'},
+						  {'c','c','c','c','c','c','c','c','c','c','c','c'},
+						  {'c','c','c','c','c','c','c','c','c','c','c','c'},
+						  {'c','c','c','c','c','c','c','c','c','c','c','c'},
+						  {'c','c','c','c','c','c','c','c','c','c','c','c'},
+						  {'c','c','c','c','c','c','c','c','c','c','c','c'},
+						  {'c','c','c','c','c','c','c','c','c','c','c','c'},
+						  {'c','c','c','c','c','c','c','c','c','c','c','c'},
+						  {'c','c','c','c','c','c','c','c','c','c','c','c'},
+						  {'c','c','c','c','c','c','c','c','c','c','c','c'},
+						  {'c','c','c','c','c','c','c','c','c','c','c','c'}
+						};
+}
