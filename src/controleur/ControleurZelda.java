f9ce1a51979a3bf57718b04f79d5da9fd849e605
LUCAS QUEMENER <lquemener@iut.univ-paris8.fr>
2018-05-14 14:56:16 +0200

    ajout murs

--- src/controleur/ControleurZelda.java
+++ src/controleur/ControleurZelda.java
@@ -12,15 +12,17 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.TilePane;
 import javafx.scene.layout.VBox;
-import modele.Plateau;
 
 public class ControleurZelda implements Initializable{
 	
 	@FXML
     private TilePane tuiles = new TilePane();
 	
-	String efrefr = new File("src/vue/tileset.png").toURI().toString();
-	 Image image = new Image(efrefr);
+	String mur = new File("src/vue/mur.png").toURI().toString();
+	 Image imgmur = new Image(mur);
+	 
+	String sol = new File("src/vue/sol.png").toURI().toString();
+	 Image imgsol = new Image(sol);
 
 	
 
@@ -31,9 +33,15 @@ public class ControleurZelda implements Initializable{
 		modele.Plateau.Plateau unPlateau = new modele.Plateau.Plateau();
 		for(int x = 0; x<12; x++) {
 			for(int y = 0; y<12; y++) {
-				if(unPlateau.getLePlateau()[x][y] == 'm') {
-					System.out.println(unPlateau.getLePlateau()[x][y]);
-					ImageView temp = new ImageView(image);
+				if(unPlateau.getPlateau()[x][y] == 'm') {
+					System.out.println(unPlateau.getPlateau()[x][y]);
+					ImageView temp = new ImageView(imgmur);
+					temp.setViewport(new Rectangle2D(0, 0, 16, 16));
+					tuiles.getChildren().add(temp);
+				}
+				else if(unPlateau.getPlateau()[x][y] == 's') {
+					System.out.println(unPlateau.getPlateau()[x][y]);
+					ImageView temp = new ImageView(imgsol);
 					temp.setViewport(new Rectangle2D(0, 0, 16, 16));
 					tuiles.getChildren().add(temp);
 				}
