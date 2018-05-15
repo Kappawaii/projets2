c2033ff8b90e8170b0f95c64da60a681c5bd8e12
MELANIE SEAN <msean@b113-10.iut.univ-paris8.fr>
2018-05-15 15:19:23 +0200

    personnage

---
+++ src/modele/Coordonnee/Coordonne.java
@@ -0,0 +1,20 @@
+package modele.Coordonnee;
+
+public class Coordonne {
+
+	private int x;
+	private int y;
+	
+	public Coordonne (int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public int getX() {
+		return this.x;
+	}
+	
+	public int getY() {
+		return this.y;
+	}
+}
