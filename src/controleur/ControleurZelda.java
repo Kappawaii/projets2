8bc2f3f40d1df9cbf5861b52affe8ab4cf157f44
LUCAS QUEMENER <lquemener@iut.univ-paris8.fr>
2018-05-17 15:38:58 +0200

    merge

--- src/controleur/ControleurZelda.java
+++ src/controleur/ControleurZelda.java
@@ -1,18 +1,16 @@
 package controleur;
 
 
-import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.geometry.Rectangle2D;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.TilePane;
+import modele.Case.Case;
 import modele.Plateau.Plateau;
+import modele.Tileset.Tileset;
 
 public class ControleurZelda implements Initializable{
 	
@@ -23,7 +21,9 @@ public class ControleurZelda implements Initializable{
     
     
 	static int scale = 4;
-	Image img = new Image(new File("tilesets/tileset0.png").toURI().toString(),968*scale,526*scale,true,true);
+	
+	Tileset tileset = new Tileset("tilesets/tileset0.png",scale, 968, 526);
+	//Image tileset1 = new Image(new File("tilesets/tileset0.png").toURI().toString(),968*scale,526*scale,true,true);
 	 
 	 
 	 public void initialize(URL location, ResourceBundle resources) {
@@ -37,30 +37,33 @@ public class ControleurZelda implements Initializable{
 		}
 	 
 
-	 public void keyPressed(KeyEvent e) {
+	 public void mouseClicked() {
 
+		 System.out.println("click");
+	 }
+	 
+       void keyPressed(KeyEvent e) {
 	       System.out.println(e.getCode());
-	   
-//	       if (e.getCode().equals(KeyCode.RIGHT)) {
-//	            System.out.println("Right key pressed");
-//	        }
-//	        else if (e.getCode().equals(KeyCode.LEFT)) {
-//	            System.out.println("Left key pressed");
-//	        }
-//	        else if (e.getCode().equals(KeyCode.UP)) {
-//	            System.out.println("Left key pressed");
-//	        }
-//	        else if (e.getCode().equals(KeyCode.DOWN)) {
-//	            System.out.println("Left key pressed");
-//	        }
-
-	       
-	    }
+    	   
+	       if (e.getCode().equals(KeyCode.RIGHT)) {
+	            System.out.println("Right key pressed");
+	        }
+	        else if (e.getCode().equals(KeyCode.LEFT)) {
+	            System.out.println("Left key pressed");
+	        }
+	        else if (e.getCode().equals(KeyCode.UP)) {
+	            System.out.println("Left key pressed");	        }
+	        else if (e.getCode().equals(KeyCode.DOWN)) {
+	            System.out.println("Left key pressed");
+	        }
+		 
+       }
 	 
 	 //Afficher l'image
 	 void afficherImage(int i) {
 		 	int id = i-1;
-			tuiles.getChildren().add(temp);
+		 	//tuiles.getChildren().add(new ImageView(new Image(new File("tilesets/tileset0.png").toURI().toString())));
+			tuiles.getChildren().add(new Case(tileset,id,scale).getImageView());
 			
 	 }
 	 
