bb575b2c5d9ef14fa58a83b6b226a17d584060a4
msean <msean@b113-06.iut.univ-paris8.fr.iut.univ-paris8.fr>
2018-06-15 14:58:15 +0200

    PathFinding BFS

--- src/modele/chemin/Node.java
+++ src/modele/chemin/Node.java
@@ -7,12 +7,15 @@ public class Node {
 
     Node avant;
     Cellule cord;
+    boolean dejaVisité;
     
     
     public Node(Node avant, Cellule cord) {
         this.avant=avant;
         this.cord=cord;
+        this.dejaVisité = false;
     }
+    
     
     public Node getAvant() {
         return avant;
@@ -22,8 +25,22 @@ public class Node {
     public Cellule getCord() {
         return cord;
     }
+    
+    public void setVisite() {
+    	this.dejaVisité = true;
+    }
+    
+    public boolean getDejaVisité() {
+    	return this.dejaVisité;
+    }
+    
+    public boolean equals(Node other) {
+    	if(this.cord.getPos()==other.cord.getPos()) {
+    		return true;
+    	}
+    	return false;
+    }
 
 
     
 }
-
