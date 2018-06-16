3aef18c296286b04fa4a13422df28b83f7c7d2b3
Miradey <melaniesean24@gmail.com>
2018-06-16 18:46:59 +0200

    methode jouer classe Plante

--- src/modele/personnage/ennemis/Plante.java
+++ src/modele/personnage/ennemis/Plante.java
@@ -4,12 +4,22 @@ import modele.Modele;
 import modele.animation.Animation;
 import modele.coordonnee.Coordonnee;
 import modele.personnage.Personnage;
+import modele.personnage.joueur.Joueur;
 import vue.tileset.Tileset;
 
 public class Plante extends Personnage {
 	
 	public Plante(String nom, Coordonnee position, int vitesse, Tileset tileset, Animation a, Modele modele) {
 		super(5, position, 16, vitesse, a, modele);
+	}
+	
+	public void jouer(Joueur joueur) {
+		
+		if(joueur.getPosition().getX()/16 >= (this.getPosition().getX()/16)-3 && (joueur.getPosition().getX())/16 <= (this.getPosition().getX()/16)+3 && // attaque à partir d'une
+		   joueur.getPosition().getY()/16 >= (this.getPosition().getY()/16)-3 && (joueur.getPosition().getY())/16 <= (this.getPosition().getY()/16)+3) { // certaine distance
+			this.attaque(joueur);	
+		}
+		
 	}
 	
 	public void attaque(Personnage p) {
