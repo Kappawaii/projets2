dbaffdad4671b893a16579335af691e70ceb2694
LUCAS QUEMENER <lquemener@iut.univ-paris8.fr>
2018-06-16 20:41:54 +0200

    Fix Gobelin

--- src/modele/personnage/ennemis/Gobelin.java
+++ src/modele/personnage/ennemis/Gobelin.java
@@ -10,56 +10,59 @@ import modele.cellule.Cellule;
 import modele.chemin.PathFinding;
 import modele.coordonnee.Coordonnee;
 import modele.personnage.Personnage;
-import modele.personnage.joueur.Joueur;
 import modele.plateau.Plateau;
 
 public class Gobelin extends Personnage {
-	
-	
+
+
 	public Gobelin(String nom, int pv, Coordonnee position, int taille, Animation a, Modele modele) {
 		super(pv, position, taille, 1, a, modele);
+		super.collider.setParent(this);
 	}
-	
+	//TODO Polymorphism
+//	@Override
 	public void jouer() {
-		Joueur joueur = modele.getJoueur();
-		Plateau map = this.modele.getCurrentNiveau().getPlateau();
-		PathFinding path = new PathFinding(this.modele, 
-							map.getCellule(this.getPosition().getX()/16, this.getPosition().getY()/16),
-							map.getCellule(joueur.getPosition().getX()/16, joueur.getPosition().getY()/16));
-		ArrayList<Cellule> pathToTheHero = path.chemin();
-		ArrayList<Input> inputs = new ArrayList<Input>();
-		if(pathToTheHero != null && !pathToTheHero.isEmpty() && isActive && inputs != null) {
-		
-//			//actions.add(Axe.DROITE);
-//			inputs.add(Input.randomMovement());
-			
-			for (int i = 0; i < pathToTheHero.size(); i++) {
-				if(pathToTheHero.get(i).getPos().getX()/16 == (this.getPosition().getX()/16)+1) {			//Droite
-					inputs.add(Input.DROITE);
+//		System.out.println(pv);
+//		System.out.println(isActive);
+		if(isActive) {
+			Plateau map = this.modele.getCurrentNiveau().getPlateau();
+			PathFinding path = new PathFinding(this.modele, 
+					map.getCellule(this.getPosition().getX()/16, this.getPosition().getY()/16),
+					map.getCellule(modele.getJoueur().getPosition().getX()/16, modele.getJoueur().getPosition().getY()/16));
+			ArrayList<Cellule> pathToTheHero = path.chemin();
+			ArrayList<Input> inputs = new ArrayList<Input>();
+
+			if(pathToTheHero != null && !pathToTheHero.isEmpty()) {	
+
+
+				//			//actions.add(Axe.DROITE);
+				//			inputs.add(Input.randomMovement());
+
+				for (int i = 0; i < pathToTheHero.size(); i++) {
+					if(pathToTheHero.get(i).getPos().getX()/16 == (this.getPosition().getX()/16)+1) {			//Droite
+						inputs.add(Input.DROITE);
+					}
+					if(pathToTheHero.get(i).getPos().getX()/16 == (this.getPosition().getX()/16)-1) {			//Gauche
+						inputs.add(Input.GAUCHE);
+					}
+					if(pathToTheHero.get(i).getPos().getY()/16 == (this.getPosition().getY()/16)-1) {			//Haut
+						inputs.add(Input.HAUT);
+					}
+					if(pathToTheHero.get(i).getPos().getY()/16 == (this.getPosition().getY()/16)+1) {			//Bas
+						inputs.add(Input.BAS);
+					}
 				}
-				if(pathToTheHero.get(i).getPos().getX()/16 == (this.getPosition().getX()/16)-1) {			//Gauche
-					inputs.add(Input.GAUCHE);
+
+				int[] movInputs = getMovements(inputs, vitesse);
+
+				//on passe le déplacement si il n'y a pas de mouvement
+				if (movInputs[0] != 0 || movInputs[1] != 0) {
+					//calcul de la prochaine position
+					int[] nextPosXetY = getNextPos(movInputs[0], movInputs[1]);
+					moveAndAnimate(movInputs[0], movInputs[1], nextPosXetY[0], nextPosXetY[1]);
 				}
-				if(pathToTheHero.get(i).getPos().getY()/16 == (this.getPosition().getY()/16)-1) {			//Haut
-					inputs.add(Input.HAUT);
-				}
-				if(pathToTheHero.get(i).getPos().getY()/16 == (this.getPosition().getY()/16)+1) {			//Bas
-					inputs.add(Input.BAS);
-				}
+
 			}
-			
-			int[] movInputs = getMovements(inputs, vitesse);
-				
-			//on passe le d�placement si il n'y a pas de mouvement
-			if (movInputs[0] != 0 || movInputs[1] != 0) {
-				//calcul de la prochaine position
-				int[] nextPosXetY = getNextPos(movInputs[0], movInputs[1]);
-				moveAndAnimate(movInputs[0], movInputs[1], nextPosXetY[0], nextPosXetY[1]);
-			}
-			
 		}
-	}
-	public void attaque(Personnage p) {
-		System.out.println("Mon ma�tre est SA-ROUU-MAAAAANE");
 	}
 }
